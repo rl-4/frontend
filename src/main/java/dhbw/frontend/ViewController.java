679a02035df245cc7ae084692156c033566608e3
rl-4 <73071274+rl-4@users.noreply.github.com>
2021-03-08 16:53:05 +0100

    added sample in ViewController.java

--- src/main/java/dhbw/frontend/ViewController.java
+++ src/main/java/dhbw/frontend/ViewController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import services.HttpSearch;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ViewController {
@@ -18,9 +20,12 @@ public class ViewController {
     @GetMapping("/test")
     public String getIndex(){
         try{
-            URI uri = new URI("http://localhost:8080/fullTextSearch");
-            FilterQuery filterQuery = new FilterQuery();
+            Map<String, String> map = new HashMap<>();
+            map.put("Category", "A");
+            FilterQuery filterQuery = new FilterQuery(true, "Test", map);
             String jsonFilterQuery = parser.filterQueryToJson(filterQuery);
+
+            URI uri = new URI("http://localhost:8080/fullTextSearch");
             httpSearch.searchPost(uri, jsonFilterQuery);
         } catch (Exception e){
 
