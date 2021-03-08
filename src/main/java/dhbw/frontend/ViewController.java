777178c9dab4db8201b0814ae4fc916889544b55
rl-4 <73071274+rl-4@users.noreply.github.com>
2021-03-08 19:12:03 +0100

    added index

--- src/main/java/dhbw/frontend/ViewController.java
+++ src/main/java/dhbw/frontend/ViewController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import services.HttpSearch;
 import view.SearchForm;
 
@@ -48,9 +49,25 @@ public class ViewController {
     }
 
     @GetMapping(value = {"/", "/index"})
-    public String getIndex(Model model) {
+    public String getIndex(Model model, @RequestParam(value = "key", required = false) String key, @RequestParam(value = "value", required = false) String value) {
         SearchForm searchForm = new SearchForm();
         model.addAttribute(searchForm);
         return "index";
+    }
+
+    @GetMapping("/upload")
+    public String uploadForm(){
+        return "upload";
+    }
+
+    @PostMapping("/upload")
+    public String uploadDocument(){
+        try{
+            //TODO uri correct?
+            URI uri = new URI("http://fileservice:8080/api/addDocument");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "upload";
     }
 }
